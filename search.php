b633f500b71e01bfecf00e96edbab228f3ad7e48
Anand Kumar Mainali <urajmainali5@gmail.com>
2018-12-08 12:43:38 +0545

    Returned typed value from searchbar into success.

--- search.php
+++ search.php
@@ -2,7 +2,10 @@
 
     $connection = mysqli_connect('localhost','root','','ajax');
 
-    if($connection)
-    {
-        echo "Connection is Successfull.";
-    }
+    // if($connection)
+    // {
+    //     echo "Connection is Successfull.";
+    // }
+
+    $search = $_POST['search'];
+    echo $search;
